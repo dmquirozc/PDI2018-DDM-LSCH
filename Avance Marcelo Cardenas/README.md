fec4654c0fb032f90094a25fdb0ec8c2d48d6550
marcelo <marcelo.cardenas.13@sansano.usm.cl>
2018-11-24 10:37:05 -0300

    Añadido trackbar de calibración. Se cambia modo umbral desde GRAY por HSV, ya que es robusto a luminosidad

--- Avance Marcelo Cardenas/README.md
+++ Avance Marcelo Cardenas/README.md
@@ -1,2 +1,6 @@
 # Avance Marcelo Cárdenas
 Carpeta donde se muestra el avance del miembro del equipo Marcelo Cárdenas.
+
+## Modo de uso
+
+python ./opencv.py
--- Avance Marcelo Cardenas/opencv.py
+++ Avance Marcelo Cardenas/opencv.py
@@ -4,23 +4,48 @@ import cv2
 import math
 
 #LOADING HAND CASCADE
-hand_cascade = cv2.CascadeClassifier('Hand_haar_cascade.xml')
+#hand_cascade = cv2.CascadeClassifier('Hand_haar_cascade.xml')
+def on_change(val):
+	return None
+
+low_thresh = 0
+high_thresh = 255
 
 # VIDEO CAPTURE
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+winTitle = "Ventana de calibración"
+tbTitle_L = "Umbral inferior"
+tbTitle_H = "Umbral superior"
+
+cv2.namedWindow(winTitle)
+cv2.createTrackbar(tbTitle_L, winTitle , 0, 255, on_change)
+cv2.createTrackbar(tbTitle_H, winTitle, 255, 255, on_change)
 
 while 1:
-	ret, img = cap.read()
+	__, img = cap.read()
 	blur = cv2.GaussianBlur(img,(5,5),0) # BLURRING IMAGE TO SMOOTHEN EDGES
-	gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY) # BGR -> GRAY CONVERSION
-	retval2,thresh1 = cv2.threshold(gray,100,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU) # THRESHOLDING IMAGE
-	hand = hand_cascade.detectMultiScale(thresh1, 1.3, 5) # DETECTING HAND IN THE THRESHOLDE IMAGE
-	mask = np.zeros(thresh1.shape, dtype = "uint8") # CREATING MASK
-	for (x,y,w,h) in hand: # MARKING THE DETECTED ROI
+	gray = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) # BGR -> GRAY CONVERSION
+
+	low_thresh = cv2.getTrackbarPos(tbTitle_L, winTitle)
+	high_thresh = cv2.getTrackbarPos(tbTitle_H, winTitle)
+
+	__, bin = cv2.threshold(gray, low_thresh, high_thresh, cv2.ADAPTIVE_THRESH_MEAN_C) # THRESHOLDING IMAGE
+
+	h, s, v = cv2.split(bin) # Se usa el canal de saturación ya que es inmune a la luminosidad
+
+	#hand = hand_cascade.detectMultiScale(thresh1, 1.3, 5) # DETECTING HAND IN THE THRESHOLDE IMAGE
+	#mask = np.zeros(thresh1.shape, dtype = "uint8") # CREATING MASK
+
+	cv2.imshow('Grises', gray)
+	cv2.imshow('Binaria', s)
+
+
+	'''for (x,y,w,h) in hand: # MARKING THE DETECTED ROI
 		cv2.rectangle(img,(x,y),(x+w,y+h), (122,122,0), 2)
 		cv2.rectangle(mask, (x,y),(x+w,y+h),255,-1)
+
 	img2 = cv2.bitwise_and(thresh1, mask)
 	final = cv2.GaussianBlur(img2,(7,7),0)
 	im, contours, hierarchy = cv2.findContours(final, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,7 +53,7 @@ while 1:
 	cv2.drawContours(img, contours, 0, (255,255,0), 3)
 	cv2.drawContours(final, contours, 0, (255,255,0), 3)
 
-	'''if len(contours) > 0:
+	if len(contours) > 0:
 		cnt=contours[0]
 		hull = cv2.convexHull(cnt, returnPoints=False)
 		# finding convexity defects
@@ -60,14 +85,14 @@ while 1:
 			cv2.putText(img,"This is 4", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
 		elif count_defects == 4:
 			cv2.putText(img,"THIS IS 5", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-        '''
+    '''
 
-        cv2.imshow('aa', gray)
-        cv2.imshow('img', thresh1)
+
 
 
 	k = cv2.waitKey(30) & 0xff
 	if k == 27:
 		break
+
 cap.release()
 cv2.destroyAllWindows()
