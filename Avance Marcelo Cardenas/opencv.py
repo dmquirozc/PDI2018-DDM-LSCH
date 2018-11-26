e5f62f2a28831d612c86695e7216a4be8392cb02
marcelo <marcelo.cardenas.13@sansano.usm.cl>
2018-11-25 21:15:36 -0300

    Detector mas robusto. Ahora detecta puños y otras figuras

--- Avance Marcelo Cardenas/opencv.py
+++ Avance Marcelo Cardenas/opencv.py
@@ -5,6 +5,7 @@ import math
 
 #LOADING HAND CASCADE
 hand_cascade = cv2.CascadeClassifier('Hand_haar_cascade.xml')
+fist_cascade = cv2.CascadeClassifier('aGest.xml')
 
 def on_change(val):
 	return None
@@ -26,14 +27,15 @@ cv2.namedWindow(winTitle)
 cv2.createTrackbar(tbTitle_L, winTitle , 35, 255, on_change)
 cv2.createTrackbar(tbTitle_H, winTitle, 255, 255, on_change)
 cv2.createTrackbar(x_offsetT, winTitle, 35, 100, on_change)
-cv2.createTrackbar(y_offsetT, winTitle, 50, 100, on_change)
+cv2.createTrackbar(y_offsetT, winTitle, 31, 100, on_change)
 cv2.createTrackbar(w_offsetT, winTitle, 26, 100, on_change)
 cv2.createTrackbar(h_offsetT, winTitle, 35, 100, on_change)
 
 while 1:
 	__, img = cap.read()
 	blur = cv2.GaussianBlur(img,(5,5),0) # BLURRING IMAGE TO SMOOTHEN EDGES
-	gray = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) # BGR -> GRAY CONVERSION
+	hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) # BGR -> GRAY CONVERSION
+	gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
 	low_thresh = cv2.getTrackbarPos(tbTitle_L, winTitle)
 	high_thresh = cv2.getTrackbarPos(tbTitle_H, winTitle)
@@ -42,14 +44,16 @@ while 1:
 	w_offset = cv2.getTrackbarPos(w_offsetT, winTitle)
 	h_offset = cv2.getTrackbarPos(h_offsetT, winTitle)
 
-	__, bin = cv2.threshold(gray, low_thresh, high_thresh, cv2.ADAPTIVE_THRESH_MEAN_C) # THRESHOLDING IMAGE
+	__, bin = cv2.threshold(hsv, low_thresh, high_thresh, cv2.ADAPTIVE_THRESH_MEAN_C) # THRESHOLDING IMAGE
 
 	h, s, v = cv2.split(bin) # Se usa el canal de saturación ya que es inmune a la luminosidad
 
 	kernel = np.ones((3,3),np.uint8)
 
 	eroded = cv2.erode(s, kernel, 1)
-	hand = hand_cascade.detectMultiScale(s, 1.3, 5) # DETECTING HAND IN THE THRESHOLDE IMAGE
+
+	hand = hand_cascade.detectMultiScale(bin, 1.1, 5) # DETECTING HAND IN THE THRESHOLDE IMAGE
+	fist = fist_cascade.detectMultiScale(gray, 1.1, 5)
 
 	binary = eroded.copy()
 
@@ -59,6 +63,12 @@ while 1:
 	for (x,y,w,h) in hand: # MARKING THE DETECTED ROI
 		cv2.rectangle(canvas, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, -1)
 		cv2.rectangle(binary, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, 1)
+		cv2.rectangle(img, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, 1)
+
+	for (x,y,w,h) in fist: # MARKING THE DETECTED ROI
+		cv2.rectangle(canvas, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, -1)
+		cv2.rectangle(binary, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, 1)
+		cv2.rectangle(img, (x + x_offset-50, y + y_offset-50), (x+w+w_offset,y+h+h_offset), 255, 1)
 
 	masked_hand = cv2.bitwise_and(canvas, eroded)
 
@@ -75,8 +85,6 @@ while 1:
 		if(area > max_area):
 			max_area = area
 			max_ind = i
-
-
 
 	if(len(contours) > 0):
 
@@ -99,7 +107,7 @@ while 1:
 			FarDefect.append(far)
 			cv2.line(hand_trace,start,end,[0,255,0],1)
 			cv2.circle(hand_trace,far,10,[100,255,255],3)
-
+	
 
 	cv2.imshow('Original', img)
 	cv2.imshow('Binaria', binary)
