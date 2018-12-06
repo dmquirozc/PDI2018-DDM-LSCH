df82c53ba28371c4acbd95289a5ea4d7244b3133
IgnacioGaete <ignacio.gaete.13@sansano.usm.cl>
2018-12-06 18:03:36 -0300

    svm simple example (update)

---
+++ Avance Ignacio Gaete/svm_example/CMakeFiles/SVM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SVM.dir/sources/main.cpp.o"
+  "SVM.pdb"
+  "SVM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
